59e0b2246f72f58a09ca7ab5bff8bdffd29f8f0d
Philip Rodriguez <ptr5201@rit.edu>
2019-04-11 22:14:23 -0400

    added hardcoded message substrings and suggested responses to use for the transcripts in our usability experiments

--- service/src/main/java/edu/rit/se/a11y/autoresponses/service/impl/SuggestedResponseServiceImpl.java
+++ service/src/main/java/edu/rit/se/a11y/autoresponses/service/impl/SuggestedResponseServiceImpl.java
@@ -26,14 +26,22 @@ public class SuggestedResponseServiceImpl implements SuggestedResponseService {
     }
 
     private void initializeContextPhrasesAndResponses() {
-        contextPhrasesMap.put("food service", Arrays.asList("what would you like to order"));
+        contextPhrasesMap.put("food service", Arrays.asList("what would you like to order", "what would you like on that", "anything to drink",
+                "that will be", "here's your receipt", "your order should be ready"));
+        contextPhrasesMap.put("movie service", Arrays.asList("what movie would you like to see", "how many tickets", "any snacks or drinks",
+                "that will be", "here are your tickets", "have a good evening"));
 
-        contextSuggestedResponsesMap.put("food service", Arrays.asList("Could I please get a cheeseburger?", "Could I please get a chicken sandwich?"));
+        contextSuggestedResponsesMap.put("food service", Arrays.asList("Could I please get a cheeseburger?",
+                "Could I please get a chicken finger sandwich?", "Cheese and honey mustard, please.", "Just water, please.", "Thank you!"));
+        contextSuggestedResponsesMap.put("movie service",
+                Arrays.asList("I would like to see", "1 adult ticket and 1 child ticket.", "Could I please get a chocolate bar?",
+                        "Could I please get a small popcorn?", "No thanks.", "Thanks, you too!", "Thanks!", "Thank you!"));
     }
 
     @Override
     public GetSuggestedResponsesResponse getSuggestedResponses(GetSuggestedResponsesRequest suggestedRequest) {
-        // Set up a default, empty response object in case there is an issue with the request body
+        // Set up a default, empty response object in case there is an issue with the
+        // request body
         GetSuggestedResponsesResponse suggestedResponse = new GetSuggestedResponsesResponse(new ArrayList<>());
 
         if (suggestedRequest != null) {
